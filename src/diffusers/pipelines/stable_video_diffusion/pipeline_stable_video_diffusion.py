376a89e1ea7b892f9fcaa6c2831b7b2aaafda0d4
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2023-12-28 16:55:01 +0100

    fix typo

--- src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
+++ src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
@@ -121,7 +121,7 @@ class StableVideoDiffusionPipeline(DiffusionPipeline):
         if not isinstance(text, torch.Tensor):
             text = self.tokenizer(text, padding=True, return_tensors="pt")
         text = text.to(device=device)
-        image_embeddings = self.text_encoder(**inputs).text_embeds.unsqueeze(1).to(dtype)
+        image_embeddings = self.text_encoder(**text).text_embeds.unsqueeze(1).to(dtype)
 
         # duplicate image embeddings for each generation per prompt, using mps friendly method
         bs_embed, seq_len, _ = image_embeddings.shape
